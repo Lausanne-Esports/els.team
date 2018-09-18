00492c66150dd2f9baff7cf2f4e171f0507f480c
Romain Lanz <romain.lanz@slynova.ch>
2018-09-19 00:03:23 +0200

    fix(ads): remove title

--- components/Advertisement/AdvertisementBlock.vue
+++ components/Advertisement/AdvertisementBlock.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="wrapper dark-blue pt-5 pb-5">
     <div class="container">
-      <div class="head d-flex mb-4 justify-content-between">
+      <!-- <div class="head d-flex mb-4 justify-content-between">
         <div class="d-flex">
           <h2>Publicité</h2>
         </div>
-      </div>
+      </div> -->
 
       <div class="row">
         <div class="col-md-8 offset-md-2">
