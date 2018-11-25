6d9fabfc98d9b5e74376dc32c3b4cedba8491b16
Killian Tornese <killian.tornese@gmail.com>
2018-11-25 18:47:16 +0100

    chore(awards): accordion added to the awards lists

--- components/Awards/AwardsGame.vue
+++ components/Awards/AwardsGame.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="award-game">
-    <div class="header">
+    <div class="header" @click="toggleList">
       <h2><i :class="'icon-' + game.code"></i> {{ game.name }}</h2>
       <div class="picture" :class="game.code"></div>
     </div>
-    <div class="awards">
+    <div ref="list" class="awards" :class="{ open }">
       <award-item :award="award" v-for="(award, index) in game.awards" :key="index"></award-item>
+      <div class="more" @click="toggleList">En voir plus</div>
     </div>
   </div>
 </template>
@@ -15,7 +16,33 @@ import AwardItem from '@/components/Awards/AwardItem';
 
 export default {
   props: ['game'],
-  components: { AwardItem }
+  data() {
+    return {
+      open: false,
+      hasMore: true,
+    }
+  },
+  components: { AwardItem },
+  mounted() {
+    if(this.$refs.list.scrollHeight <= this.$refs.list.clientHeight) {
+      this.open = true;
+      this.hasMore = false;
+    }
+  },
+  methods: {
+    toggleList() {
+      if(!this.hasMore)
+        return;
+
+      if (this.$refs.list.style.maxHeight){
+        this.$refs.list.style.maxHeight = null;
+        this.open = false;
+      } else {
+        this.$refs.list.style.maxHeight = this.$refs.list.scrollHeight + "px";
+        this.open = true;
+      }
+    }
+  }
 }
 </script>
 
