320ee3cf9f0258c42d3baa9945fe94e995eca1b4
Romain Lanz <romain.lanz@slynova.ch>
2019-04-03 21:25:23 +0200

    chore: bump version

--- components/Layout/CopyrightBlock.vue
+++ components/Layout/CopyrightBlock.vue
@@ -2,7 +2,7 @@
   <div class="wrapper darker-blue">
     <div class="container copyright pb-4 pt-4 d-flex justify-content-between">
       <p class="els">2019 &copy; Lausanne-Sport eSports</p>
-      <p><small>v.20190228-2</small><span class="mx-2">|</span>Design: <a href="http://louisburgard.com/" target="blank">Louis "biip" Burgard</a></p>
+      <p><small>v.20190403</small><span class="mx-2">|</span>Design: <a href="http://louisburgard.com/" target="blank">Louis "biip" Burgard</a></p>
     </div>
   </div>
 </template>
