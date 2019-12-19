e2c8f929a1d13b73eaad951a6d2deb4085fea87d
Romain Lanz <romain.lanz+github@pm.me>
2019-12-19 23:47:07 +0100

    chore: remove sponsor

--- components/Layout/SponsorBlock.vue
+++ components/Layout/SponsorBlock.vue
@@ -1,12 +1,9 @@
 <template>
   <section class="wrapper darker-blue pt-5 pb-5">
     <div class="container sponsors">
-      <div class="mb-5">
+      <div>
         <a href="https://www.andros.ch/" target="blank" class="main"><img v-lazy="'/images/sponsors/andros.svg'" alt="Andros"></a>
         <a href="https://www.casinosbarriere.com/fr/montreux.html" target="blank" class="main"><img v-lazy="'/images/sponsors/barriere.svg'" alt="Casino Barrière"></a><br />
-      </div>
-      <div>
-        <a href="http://tech-center.ch/" target="blank"><img v-lazy="'/images/sponsors/tcm.svg'" alt="Tech-Center"></a>
       </div>
     </div>
   </section>
