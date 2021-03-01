381e43ba4ec8a4981a60b224212af5f37adaa053
Valentin Kaelin <kalanehd@gmail.com>
2021-03-01 22:07:16 +0100

    feat: update about page

--- pages/about.vue
+++ pages/about.vue
@@ -12,29 +12,34 @@
     </header>
 
     <div class="container body">
-      <p>Le Lausanne-Sport eSports est <strong>l'équipe Esportive officielle du club de football phare de Lausanne</strong> (en Suisse), nous avons pour ambition de remporter les tournois suisses et européens ainsi que de <strong>développer le monde du sport éléctronique</strong>.</p>
+      <p>Nous sommes le Lausanne-Sport Esport, une association à but non lucratif évoluant dans le monde de l’esport suisse depuis le 6 avril 2017.</p>
 
-      <p>Historiquement nommé Qualitas Helvetica, l’équipe décide de s’allier avec un partenaire de qualité en concluant une fusion avec le Lausanne-Sport, et deviendra le Lausanne-Sport Esports. C’est officiellement le 6 avril 2017 que l’annonce de création du Lausanne-Sport esports est faite aux médias et à la communauté gaming, après plusieurs mois de travail qui ont été nécessaire pour préparer au mieux cette incroyable aventure.</p>
+      <p>Avant de s’allier avec le FC Lausanne-Sport, notre association se nommait Qualitas Helvetica. Celle-ci a été créée le 1er août 2015, avec à sa tête notre président actuel, Frédéric «Torrix» Boy. Par la suite, deux anciennes associations ont rejoint nos rangs : la Team Evenfall en août 2018 et Night Saber en février 2019. Aujourd’hui, nous sommes l’un des clubs les plus titrés de Suisse, avec plus de 150 podiums sur dix jeux différents !</p>
 
-      <p>Lors de notre premier tournoi sous les couleurs du club, Polylan 29 , nous nous illustreront avec deux titres de champions et un de vice-champion. Suivant cette voie pendant nos deux premières années - <strong>nous comptabilisons un total de 68 trophées sur 6 jeux</strong>, ainsi que 6 résultats en tournois européens ou de prestige.</p>
+      <p>Nous avons pour ambition de promouvoir, dynamiser et professionnaliser l’esport en Suisse, ainsi que de remporter des tournois nationaux et internationaux. Nous faisons tout notre possible pour qu’un jour, l’esport soit une discipline reconnue dans le paysage helvétique.</p>
 
       <figure class="wild">
         <img v-lazy="`https://i.imgur.com/Ge0FVPf.jpg`" alt="Polylan 29">
         <figcaption>L'équipe Overwatch Pachimari gagnant la Polylan 29</figcaption>
       </figure>
 
+      <p>Pour accéder à ce but, nous organisons des tournois pour aider la scène à grandir et nous formons la jeunesse en offrant la meilleure structure possible du pays à nos joueurs talentueux qui sont pour la plupart romands ou suisses. Des talents étrangers complètent parfois nos équipes afin de nous apporter expertise et expérience.</p>
+
+      <p>En complément à notre activité sur la scène esportive, nous réalisons et diffusons du contenu lors d’évènements, et produisons des talkshows sur notre chaîne Twitch «Lausanne TV».</p>
+
+      <p>Tout cela serait bien évidemment impossible sans l’aide de nos nombreux bénévoles ; psychologues, rédacteurs, community managers, coaches, staffs événementiel, designers, développeurs web, photographes et monteurs vidéos qui nous aident et tirent à la même corde dans un but commun.</p>
+
+      <p>Mais ce n’est pas tout ! En effet, nous sommes également impliqués dans des actions caritatives. Grâce à notre collaboration avec Zoé4Life, nous sommes fiers d’avoir organisé des événements ayant pour but de lever des fonds pour une cause nous tenant à cœur. En 2020, nous avons récolté plus de 6300 francs pour la lutte contre le cancer de l’enfant !</p>
+    
+      <p>Sans partenaire ou sponsors, un club esportif ne pourrait tout simplement pas exister. Merci bien évidemment au FC Lausanne-Sport avec qui nous sommes en étroite collaboration. La marque Andros qui nous permet d’être vitaminés depuis 2018 ! Et finalement, le Casino Barrière qui nous aide lors de nos événements.</p>
+    
       <p>
-        En constante évolution, à l'instar de notre vision, notre équipe se diversifie et se rend compétente dans divers domaines&nbsp;:
-        <ul>
-          <li><strong>La formation de la jeunesse</strong> en encadrant des équipes de jeunes, sous l'appellation "Academy"</li>
-          <li><strong>La diffusion et le commentaring de tournois</strong> via LausanneTV, grâce à la fusion avec la Team Evenfall en août 2018 qui est venue renforcer les pôles liés à la communication et aux médias.</li>
-          <li><strong>L'organisation d'évènements</strong>, en ligne ainsi qu'hors ligne, grâce à la fusion avec Night Saber en février 2019.</li>
-        </ul>
-      </p>
-
-      <p>C'est avec ces nouvelles clés en main que nous nous élançons pour l'année 2019, tout en creusant notre avance sur le plan compétitif. Les résultats amenés par nos équipes, ainsi que LausanneTV - projet quasi-indépendant - qui est devenue diffuseur exclusive de la première division SeSL, montrent que <strong>le travail de chacun de nos membres et la confiance accordée par nos sponsors ont été payants</strong>.</p>
-
-      <p>Aujourd'hui, fort de notre expérience, <strong>nous souhaitons nous rapprocher du public</strong>, initié comme profane, afin de transmettre cette passion qui est nôtre&nbsp;; celle de faire grandir l'Esports suisse.</p>
+        Si vous souhaitez nous contacter, l’onglet
+        <nuxt-link to="/contact">contact</nuxt-link>
+        sur le haut de notre site internet est fait pour vous. Vous pouvez aussi nous rejoindre sur
+        <a href="https://discord.gg/TUrQwaTMhQ" target="_blank" rel="nofollow noopener noreferrer">Discord</a>,
+        notre communauté vous attend avec impatience !
+        </p>
     </div>
   </article>
 </template>
